f722954518c75c598065478a57a015114a1bbf2c
Jonah Tarver <jonahdevine@gmail.com>
2021-02-03 10:52:20 -0800

    Created migration and seed files

--- src/config.js
+++ src/config.js
@@ -1,4 +1,7 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
+    DATABASE_URL: process.env.DATABASE_URL || "postgresql://jonahtarver@localhost/rotation",
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || "postgresql://jonahtarver@localhost/rotation-test",
+    API_TOKEN: process.env.API_TOKEN
 }
